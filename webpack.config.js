b7a4eef678abd57535b86849d18f51d698d3bdaf
dwayneRichards <dwaynerichards@gmail.com>
2021-04-19 17:15:52 -0400

    commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/build/',
+    contentBase: path.resolve(__dirname, 'build'),
+    proxy: {
+      // /api/ works as well
+      '/': 'http://localhost:3000'
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.gif', '.png', '.jpg', '.jpeg', '.svg']
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: [/node_modules/, /client\/stylesheets\/modules/],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules',
+        include: /flexboxgrid/
+      }
+    ],
+  }
+};
+
+
+
+
+
