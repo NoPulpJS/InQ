4354c8d6f6d0c655652c36727a6b201e457ded1b
ktrane1 <kushal.talele@gmail.com>
2021-04-19 21:41:01 -0400

    building successful

--- server/server.js
+++ server/server.js
@@ -3,13 +3,18 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
-const loginRouter = require('./routes/login');
-
 dotenv.config();
 
 const app = express();
-
 const port = 3000;
+
+const loginRouter = require('./routes/login');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../client/')));
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
