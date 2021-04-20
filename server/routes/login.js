119d12bcd4f196e91fb6fe03158d297816e51dbb
ktrane1 <kushal.talele@gmail.com>
2021-04-20 12:13:29 -0400

    OAuth

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,32 @@
 const express = require('express');
-const OAuthController = require('../controllers/OAuthController');
+// const OAuthController = require('../controllers/OAuthController');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const passport = require('passport');
 
 const router = express.Router();
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: '/login/OAuth/callback',
+  passReqToCallback: true,
+},
+((request, accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+  // User.findOrCreate({ googleId: profile.id }, (err, user) => done(err, user));
+  return done(null, profile, accessToken);
+})));
+
+router.get('/OAuth/',
+  passport.authenticate('google', {
+    scope:
+  ['email', 'profile'],
+  }));
+
+router.get('/OAuth/callback/',
+  passport.authenticate('google', {
+    failureRedirect: '/auth/google/failure',
+  }),
+  (req, res) => res.status(200).send('wooohoooooo'));
 
 module.exports = { router };
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
@@ -8,13 +10,25 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-const loginRouter = require('./routes/login');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const loginRouter = require('./routes/login');
+
+app.use(passport.initialize());
+app.use('/login', loginRouter.router);
 app.use(express.static(path.join(__dirname, '../client/')));
+// app.use('/', (req, res, next) => res.sendFile(path.resolve(__dirname, '../client/index.html')));
+
+app.use('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send('Internal Server Error');
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
