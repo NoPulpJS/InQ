38df226b00ea9e0a89d6f69a8387d23b2d697804
dwayneRichards <dwaynerichards@gmail.com>
2021-04-22 10:47:44 -0400

    bloody hell

--- server/controllers/infoController.js
+++ server/controllers/infoController.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk');
+const db = require('../data/models');
+
 module.exports = {
   getUserInfo: (req, res, next) => {
     console.log('REQ.USER------', req.user);
@@ -10,8 +13,30 @@ module.exports = {
       photo: photo_url,
       email,
     };
-    // console.log('res.locals: ', res.locals.userInfo)
     return next();
   },
+
+  getCategories: (req, res, next) => {
+    const query = { text: 'SELECT category  FROM categories' };
+    db.query(query)
+      .then((data) => {
+        res.locals.categories = data.rows;
+        console.log(chalk.red('infocontroler'), res.locals.categories);
+        return next();
+      })
+      .catch((e) => next({
+        message: 'error',
+        error: e,
+      }));
+  },
+  // postMessageBoard: (req, res, next) => {
+  //   console.log(chalk.red('INFOCONTROLLER CONSOLELOG, REQ BODY'), req.body);
+  //   const { message } = req.message;
+  //   res.locals.messageInfo = {
+  //     message,
+  //   };
+  //   console.log(chalk.red('INFOCONTROLLER CONSOLELOG, RES LOCALS MESSAGEINFO'), res.locals.messageInfo);
+  //   return next();
+  // },
 
 };
